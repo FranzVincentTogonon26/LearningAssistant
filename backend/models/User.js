55a5e114da891bbba31d182fa35db3456a939980
FranzVincentTogonon26 <franzvincenttogonon@gmail.com>
2026-01-19 19:36:02 +0800

    Debugging Error User Hashing

--- backend/models/User.js
+++ backend/models/User.js
@@ -31,23 +31,22 @@ const userSchema = new mongoose.Schema({
 });
 
 
-//  Hash password before saving
+// //  Hash password before saving
 
-userSchema.pre('save', async function(next) {
-    if(!this.isModified('password')){
-        return next();
-    }
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
+// userSchema.pre('save', async function(next) {
+//     if(!this.isModified('password')){
+//         next();
+//     }
+//     const salt = await bcrypt.genSalt(10);
+//     this.password = await bcrypt.hash(this.password, salt);
  
-});
+// });
 
-//  Compare password method
+// //  Compare password method
 
-userSchema.methods.matchPassword = async function(enteredPassword) {
-    return await bcrypt.compare(enteredPassword, this.password);
-}
+// userSchema.methods.matchPassword = async function(enteredPassword) {
+//     return await bcrypt.compare(enteredPassword, this.password);
+// }
 
 
 const User = mongoose.model('User', userSchema);
