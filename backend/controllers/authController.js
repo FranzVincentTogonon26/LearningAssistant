f5f60d616ecffbe2a758af68e42988519ba3246a
FranzVincentTogonon26 <franzvincenttogonon@gmail.com>
2026-01-19 13:06:53 +0800

    Fixing JWT token

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -4,8 +4,9 @@ import User from '../models/User.js';
 //  Generate JWT token
 const generateToken = (id) => {
     return jwt.sign( { id }, process.env.JWT_SECRET, {
-        expiresIn: process.env.JWT_EXPIRE | "7d"
+        expiresIn: process.env.JWT_EXPIRE
     } )
+
 };
 
 
--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -9,8 +9,12 @@ const protect = async ( req, res, next ) => {
     if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
 
         try {
+
+            token = req.headers.authorization.split(' ')[1];
+
             // Verify Token
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
             req.user = await User.findById(decoded.id).select('-password');
 
             if (!req.user) {
