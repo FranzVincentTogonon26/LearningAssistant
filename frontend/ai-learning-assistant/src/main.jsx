e2e6da5a2f20bb3083e813add77189f6ef1fdf33
FranzVincentTogonon26 <franzvincenttogonon@gmail.com>
2026-01-21 23:41:43 +0800

    Impleamenting Login Page

--- frontend/ai-learning-assistant/src/main.jsx
+++ frontend/ai-learning-assistant/src/main.jsx
@@ -1,10 +1,15 @@
+import App from './App.jsx'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { AuthProvider } from './context/AuthContext.jsx'
+import { Toaster } from 'react-hot-toast'
 import './index.css'
-import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+        <Toaster position='top-center' toastOptions={{duration:3000}} />
+       <App />
+    </AuthProvider>
   </StrictMode>,
 )
